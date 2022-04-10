b1e24011b55dd522043e2849167bf561049f4c96
Masl <marcel.z2006@gmail.com>
2022-04-11 00:24:31 +0200

    testing

--- src/com/marcel/Main.java
+++ src/com/marcel/Main.java
@@ -3,6 +3,6 @@ package com.marcel;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! aaaaa");
     }
 }
